3f6c4976b7807505a77948cf67483f6764e5cac9
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-02 17:57:32 +0200

    grouped all the test together

--- script.test.js
+++ script.test.js
@@ -2,16 +2,25 @@ const googleSearch = require("./script");
 
 dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpicture.com"];
 
-it("it is a silly test", () => {
-  expect("hello").toBe("hello");
-});
+describe("searchgoogle", () => {
+  it("it is a silly test", () => {
+    expect("hello").toBe("hello");
+  });
 
-it("searching for google ", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]),
-    expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpicture.com"]);
-});
+  it("searching for google ", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]),
+      expect(googleSearch("dog", dbMock)).toEqual([
+        "dog.com",
+        "dogpicture.com"
+      ]);
+  });
 
-it("should work with undefined & null  ", () => {
-  expect(googleSearch(undefined, dbMock)).toEqual([]),
-    expect(googleSearch(null, dbMock)).toEqual([]);
+  it("should work with undefined & null  ", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]),
+      expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("should  not return more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
