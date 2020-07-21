f2792456e43f5adba9be6151abef3fae4d127d27
vimal Raveendran <vimalraveendra@gmail.com>
2020-07-21 22:43:01 +0200

    upadated package dot json

--- script2.test.js
+++ script2.test.js
@@ -3,14 +3,14 @@ const swapi = require("./script2");
 
 it("should call swapi to getpeople", () => {
   expect.assertions(1);
-  return swapi.getPeople(fetch).then(data => {
+  return swapi.getPeople(fetch).then((data) => {
     expect(data.count).toEqual(87);
   });
 });
 
 it("should call swapi to getpeople with a promise", () => {
   expect.assertions(2);
-  return swapi.getPeoplePromise(fetch).then(data => {
+  return swapi.getPeoplePromise(fetch).then((data) => {
     expect(data.count).toEqual(87);
     expect(data.results.length).toBeGreaterThan(5);
   });
@@ -22,12 +22,12 @@ it("should getPeople returns count and results", () => {
       json: () =>
         Promise.resolve({
           count: 87,
-          results: [0, 1, 2, 3, 4, 5, 6]
-        })
+          results: [0, 1, 2, 3, 4, 5, 6],
+        }),
     })
   );
   expect.assertions(4);
-  return swapi.getPeoplePromise(mockFetch).then(data => {
+  return swapi.getPeoplePromise(mockFetch).then((data) => {
     expect(mockFetch.mock.calls.length).toBe(1);
     expect(mockFetch).toBeCalledWith("https://swapi.co/api/people/");
     expect(data.count).toEqual(87);
